c84ae45d764c36c40f54cdc9775bc0eebd89a119
Eduardo Rodrigues Santana <eduardo.r.santana@hotmail.com>
2021-09-02 21:59:16 -0300

    Adicionado validação pro caso do usuário não inserir nenhum valor. É lançado um Toast avisando que a senha não foi gerada.

--- app/src/main/java/com/example/geradordesenhaaleatoria/MainActivity.kt
+++ app/src/main/java/com/example/geradordesenhaaleatoria/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.geradordesenhaaleatoria
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.Random
@@ -16,17 +17,20 @@ class MainActivity : AppCompatActivity() {
             val random = Random()
 
             val txtTamanho = txtTamanho.text.toString()
-            val tam = txtTamanho.toInt()
+            if(txtTamanho.trim() != ""){
+                val tam = txtTamanho.toInt()
+                var senhaGerada = ""
+                var index = -1
 
-            var senhaGerada = ""
-            var index = -1
+                for (i in 1..tam) {
+                    index = random.nextInt(caracteres.length)
+                    senhaGerada += caracteres.substring(index, index + 1)
+                }
 
-            for (i in 1..tam) {
-                index = random.nextInt(caracteres.length)
-                senhaGerada += caracteres.substring(index, index + 1)
+                txtFrase.text = senhaGerada
+            } else {
+                Toast.makeText(this@MainActivity, "Não foi possível gerar a senha :(", Toast.LENGTH_LONG).show()
             }
-
-            txtFrase.text = senhaGerada
         }
     }
 }
